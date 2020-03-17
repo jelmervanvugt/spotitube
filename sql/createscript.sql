451a05673b76eeea3946cfab28211b298668dfc3
jelmervanvugt <jelmervanvugt@outlook.com>
2020-03-17 15:08:53 +0100

    ddl update en nieuwe sp

--- sql/createscript.sql
+++ sql/createscript.sql
@@ -7,13 +7,15 @@ id									int							not null auto_increment,
 fullname						varchar(250)		not null,
 user							varchar(50)			not null,
 password					varchar(50)			not null,
-token							varchar(15)			null,
+token							varchar(14)			null,
 
+unique(user),
 primary key(id)
 );
-create table album (
+
+create table performer (
 id									int							not null auto_increment,
-name							varchar(250)		not null,
+name 						varchar(100)		not null,
 
 primary key(id)
 );
@@ -21,16 +23,16 @@ primary key(id)
 create table track (
 id									int							not null auto_increment,
 title								varchar(250)		not null,
-performer					varchar(100)		not null,
+performer					int							not null,
 duration						int							not null,
-album							int							null,
+album							varchar(100)		null,
 playcount					int							null,
 publicationDate			date 						null,
 description				varchar(500)		null,
 offlineAvailable			boolean				not null,
 
 primary key(id),
-foreign key(album) references album(id)
+foreign key(id) references performer(id)
 );
 
 create table playlist (
--- sql/storedprocedures.sql
+++ sql/storedprocedures.sql
@@ -1,9 +1,47 @@
 use spotitube;
 
-drop procedure if exists login;
-create procedure login(in username varchar(50), in password varchar(50))
+/* bestaat de user */
+delimiter //
+drop procedure if exists doesUserExist //
+create procedure doesUserExist (
+in 		user		 		varchar(50), 
+in 		password 	varchar(50)
+)
 begin
 
-select * from user where username = username and password = password;
+		select 			count(*) 
+		from 				user u 
+		where 			u.user = user
+		and 				u.password = password;
+        
+end //
+delimiter ;
 
-end;
+/* return de fullname van user inc. token */
+delimiter //
+drop procedure if exists getUser //
+create procedure getUser (
+in 		user		 		varchar(50), 
+in 		password 	varchar(50)
+)
+begin
+
+			update user u set token = concat(
+            cast(floor(rand()*(999-100+1)+100) as char(3)),"-", 
+            cast(floor(rand()*(999-100+1)+100) as char(3)), "-", 
+            cast(floor(rand()*(999-100+1)+100) as char(3)))
+            where u.user = user;
+            
+            select token, fullname 
+            from user u 
+            where u.user = user
+            and u.password = password;
+		
+end //
+delimiter ;
+
+
+
+
+
+
