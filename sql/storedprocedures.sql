01fe617b33e26c54b9b9811c1f2a9d76f022c1e1
jelmervanvugt <jelmervanvugt@outlook.com>
2020-03-17 15:58:49 +0100

    new stored procedure commit

--- sql/storedprocedures.sql
+++ sql/storedprocedures.sql
@@ -40,6 +40,40 @@ begin
 end //
 delimiter ;
 
+/* returned playlist id, name & owner */
+delimiter //
+drop procedure if exists getAllPlaylists //
+create procedure getAllPlaylists (
+in 		token		 		varchar(14)
+)
+begin
+
+			select 		p.id, p.name, up.owner
+            from			playlist p, userplaylist up, user u
+            where		user.token = token
+            and				user.id = up.userid
+            and				p.id = up.playlistid;
+		
+end //
+delimiter ;
+
+/* returned alles van een track */
+delimiter //
+drop procedure if exists getTrackFromPlaylist //
+create procedure getTrackFromPlaylist (
+in 		playlistid		 		int
+)
+begin
+
+			select  *
+            from track t, playlist p
+            where  t.id = p.trackid;
+		
+end //
+delimiter ;
+
+
+
 
 
 
