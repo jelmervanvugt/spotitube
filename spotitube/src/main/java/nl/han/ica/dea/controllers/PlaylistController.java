de5792ef55c1828a6b0d7fc1657cdc490d7586fd
jelmervanvugt <jelmervanvugt@outlook.com>
2020-03-19 14:44:07 +0100

    v1 all playlists

--- spotitube/src/main/java/nl/han/ica/dea/controllers/PlaylistController.java
+++ spotitube/src/main/java/nl/han/ica/dea/controllers/PlaylistController.java
@@ -1,8 +1,9 @@
 package nl.han.ica.dea.controllers;
 
-import nl.han.ica.dea.dao.PlaylistDAO;
 import nl.han.ica.dea.dao.PlaylistsDAO;
+import nl.han.ica.dea.dto.PlaylistsDTO;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -19,24 +20,20 @@ public class PlaylistController {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllPlaylists(@PathParam("token") String token) {
         Response response;
-        try {
-            playlistsDAO.initConnection();
-            response = Response
-                    .status(Response.Status.OK)
-                    .entity(new PlaylistDTO())
-                    .build();
-            playlistsDAO.closeConnection();
-        } catch (SQLException e) {
-            response = Response
-                    .status(Response.Status.BAD_REQUEST)
-                    .build();
-            e.printStackTrace();
-        }
+        playlistsDAO.initConnection();
+
+        response = Response
+                .status(Response.Status.OK)
+                .entity(new PlaylistsDAO().getAllPlaylists(token))
+                .build();
+
+        playlistsDAO.closeConnection();
         return response;
     }
 
-    private void setPlaylistDAO(PlaylistDAO playlistDAO) {
-        this.playlistDAO = playlistDAO;
+    @Inject
+    private void setPlaylistsDAO(PlaylistsDAO playlistsDAO) {
+        this.playlistsDAO = playlistsDAO;
     }
 
 }
--- spotitube/src/main/java/nl/han/ica/dea/dao/PlaylistsDAO.java
+++ spotitube/src/main/java/nl/han/ica/dea/dao/PlaylistsDAO.java
@@ -1,12 +1,12 @@
 package nl.han.ica.dea.dao;
 
 import nl.han.ica.dea.database.util.DatabaseProperties;
+import nl.han.ica.dea.dto.PlaylistDTO;
 import nl.han.ica.dea.dto.PlaylistsDTO;
+import nl.han.ica.dea.dto.TrackDTO;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class PlaylistsDAO {
 
@@ -14,8 +14,19 @@ public class PlaylistsDAO {
     private DatabaseProperties dbp = new DatabaseProperties();
     private ResultSet rs;
 
-    public PlaylistsDTO getAllPlaylists() {
+    public PlaylistsDTO getAllPlaylists(String token) {
+        PlaylistsDTO playlistsDTO = null;
+        try {
 
+            rs = queryPlaylistInfo(token);
+            ArrayList<PlaylistDTO> playlists = getPlaylistInfo();
+            int playlistLength = getPlaylistsLength(playlists);
+            playlistsDTO = new PlaylistsDTO(playlists, playlistLength);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return playlistsDTO;
     }
 
     public void closeConnection() {
@@ -33,6 +44,41 @@ public class PlaylistsDAO {
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println("Error connecting to a database: " + e);
         }
+    }
+
+    private ResultSet queryPlaylistInfo(String token) throws SQLException {
+        Statement stmt = connection.createStatement();
+        return stmt.executeQuery("call getAllPlaylists(\"" + token + "\");");
+    }
+
+    private ArrayList<PlaylistDTO> getPlaylistInfo() throws SQLException {
+        ArrayList<PlaylistDTO> playlists = new ArrayList<>();
+        while (rs.next()) {
+            playlists.add(new PlaylistDTO(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getBoolean("owner"),
+                    new ArrayList<TrackDTO>()));
+        }
+        return playlists;
+    }
+
+    private int getPlaylistsLength(ArrayList<PlaylistDTO> playlists) throws SQLException {
+        int playlistsLength = 0;
+        for (PlaylistDTO playlist : playlists) {
+            playlistsLength += getPlaylistLength(playlist);
+        }
+        return playlistsLength;
+    }
+
+    private int getPlaylistLength(PlaylistDTO playlistDTO) throws SQLException {
+        int length = 0;
+        Statement stmt = connection.createStatement();
+        rs = stmt.executeQuery("call getPlaylistLength(\"" + playlistDTO.getId() + "\");");
+        while (rs.next()) {
+            length = rs.getInt("playlistlength");
+        }
+        return length;
     }
 
 }
