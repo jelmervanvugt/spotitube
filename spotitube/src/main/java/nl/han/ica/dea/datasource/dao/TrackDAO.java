c3bc1e4c3023a09d27a5d19cfbd3252272543a0f
jelmervanvugt <jelmervanvugt@outlook.com>
2020-03-25 23:00:22 +0100

    refactor trackservice
    responsibility maken van response verplaatst van dao naar service

--- spotitube/src/main/java/nl/han/ica/dea/datasource/dao/TrackDAO.java
+++ spotitube/src/main/java/nl/han/ica/dea/datasource/dao/TrackDAO.java
@@ -6,8 +6,6 @@ import nl.han.ica.dea.controller.dto.TracksDTO;
 import nl.han.ica.dea.service.ConnectionService;
 
 import javax.inject.Inject;
-import javax.ws.rs.BadRequestException;
-import javax.ws.rs.core.Response;
 import java.sql.*;
 
 public class TrackDAO {
@@ -15,103 +13,45 @@ public class TrackDAO {
     private TracksDataMapper tracksDataMapper;
     private ConnectionService connectionService;
 
-    public Response getTracksFromPlaylist(int playlistId) {
+    public void addTrackToPlaylist(int playlistId, TrackDTO track) throws SQLException {
         connectionService.initConnection();
-        Response response = null;
-        try {
-            response = Response
-                    .status(Response.Status.OK)
-                    .entity(getAllTracksFromPlaylist(playlistId))
-                    .build();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new BadRequestException();
-        }
-        connectionService.closeConnection();
-        return response;
-    }
-
-    public Response getTracksNotInPlaylist(int playlistId) {
-        connectionService.initConnection();
-        Response response = null;
-        try {
-            response = Response
-                    .status(Response.Status.OK)
-                    .entity(getAllTracksNotInPlaylist(playlistId))
-                    .build();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new BadRequestException();
-        }
-        connectionService.closeConnection();
-        return response;
-    }
-
-    public Response addTrackToPlaylist(String token, int playlistId, TrackDTO track) {
-        connectionService.initConnection();
-        Response response = null;
-        try {
-            queryAddTrackToPlaylist(playlistId, track);
-            response = Response
-                    .status(Response.Status.OK)
-                    .entity(getAllTracksFromPlaylist(playlistId))
-                    .build();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new BadRequestException();
-        }
-        connectionService.closeConnection();
-        return response;
-    }
-
-    public Response deleteTrackFromPlaylist(String token, int playlistId, int trackId) {
-        connectionService.initConnection();
-        Response response = null;
-        try {
-            queryDeleteTrackFromPlaylist(playlistId, trackId);
-            response = Response
-                    .status(Response.Status.OK)
-                    .entity(getAllTracksFromPlaylist(playlistId))
-                    .build();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new BadRequestException();
-        }
-        connectionService.closeConnection();
-        return response;
-    }
-
-    private void queryAddTrackToPlaylist(int playlistId, TrackDTO track) throws SQLException {
         var sql = "call addTrackToPlaylist(?, ?, ?);";
         var stmt = connectionService.getConnection().prepareStatement(sql);
         stmt.setInt(1, playlistId);
         stmt.setInt(2, track.getId());
         stmt.setBoolean(3, track.getOfflineAvailable());
         stmt.executeUpdate();
+        connectionService.closeConnection();
     }
 
-    private TracksDTO getAllTracksFromPlaylist(int playlistId) throws SQLException {
+    public TracksDTO getAllTracksFromPlaylist(int playlistId) throws SQLException {
+        connectionService.initConnection();
         var sql = "call getTracksFromPlaylist(?);";
         var stmt = connectionService.getConnection().prepareStatement(sql);
         stmt.setInt(1, playlistId);
         TracksDTO tracks = tracksDataMapper.mapToDTO(stmt.executeQuery());
+        connectionService.closeConnection();
         return tracks;
     }
 
-    private TracksDTO getAllTracksNotInPlaylist(int playlistId) throws SQLException {
+    public TracksDTO getAllTracksNotInPlaylist(int playlistId) throws SQLException {
+        connectionService.initConnection();
         var sql = "call getAllTracksNotInPlaylist(?);";
         var stmt = connectionService.getConnection().prepareStatement(sql);
         stmt.setInt(1, playlistId);
         TracksDTO tracks = tracksDataMapper.mapToDTO(stmt.executeQuery());
+        connectionService.closeConnection();
         return tracks;
     }
 
-    private void queryDeleteTrackFromPlaylist(int playlistId, int trackId) throws SQLException {
+    public void deleteTrackFromPlaylist(int playlistId, int trackId) throws SQLException {
+        connectionService.initConnection();
         var sql = "call deleteTrackFromPlaylist(?, ?);";
         var stmt = connectionService.getConnection().prepareStatement(sql);
         stmt.setInt(1, playlistId);
         stmt.setInt(2, trackId);
         stmt.executeUpdate();
+        connectionService.closeConnection();
     }
 
     @Inject
--- spotitube/src/main/java/nl/han/ica/dea/service/TrackService.java
+++ spotitube/src/main/java/nl/han/ica/dea/service/TrackService.java
@@ -4,23 +4,63 @@ import nl.han.ica.dea.datasource.dao.TrackDAO;
 import nl.han.ica.dea.controller.dto.TrackDTO;
 
 import javax.inject.Inject;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.core.Response;
+import java.sql.SQLException;
 
 public class TrackService {
 
     private TrackDAO trackDAO;
 
-    public Response getTracksNotInPlaylist(int playlistId)
-    { return trackDAO.getTracksNotInPlaylist(playlistId); }
+    public Response getTracksNotInPlaylist(int playlistId) {
+        try {
+            return Response
+                    .status(Response.Status.OK)
+                    .entity(trackDAO.getAllTracksNotInPlaylist(playlistId))
+                    .build();
+        } catch(SQLException e) {
+            e.printStackTrace();
+            throw new BadRequestException();
+        }
+    }
 
-    public Response addTrackToPlaylist(String token, int playlistId, TrackDTO track)
-    { return trackDAO.addTrackToPlaylist(token, playlistId, track); }
+    public Response addTrackToPlaylist(String token, int playlistId, TrackDTO track) {
+        try {
+            trackDAO.addTrackToPlaylist(playlistId, track);
+            return Response
+                    .status(Response.Status.OK)
+                    .entity(trackDAO.getAllTracksFromPlaylist(playlistId))
+                    .build();
+        } catch(SQLException e) {
+            e.printStackTrace();
+            throw new BadRequestException();
+        }
+    }
 
-    public Response deleteTrackFromPlaylist(String token, int playlistId, int trackId)
-    { return trackDAO.deleteTrackFromPlaylist(token, playlistId, trackId); }
+    public Response deleteTrackFromPlaylist(String token, int playlistId, int trackId) {
+        try {
+            trackDAO.deleteTrackFromPlaylist(playlistId, trackId);
+            return Response
+                    .status(Response.Status.OK)
+                    .entity(trackDAO.getAllTracksFromPlaylist(playlistId))
+                    .build();
+        } catch(SQLException e) {
+            e.printStackTrace();
+            throw new BadRequestException();
+        }
+    }
 
-    public Response getTracksFromPlaylist(int playlistId)
-    { return trackDAO.getTracksFromPlaylist(playlistId); }
+    public Response getTracksFromPlaylist(int playlistId) {
+        try {
+            return Response
+                    .status(Response.Status.OK)
+                    .entity(trackDAO.getAllTracksFromPlaylist(playlistId))
+                    .build();
+        } catch(SQLException e) {
+            e.printStackTrace();
+            throw new BadRequestException();
+        }
+    }
 
     @Inject
     public void setTrackDAO(TrackDAO trackDAO) {
